e50855dd42856afb7ce73d671606074aa40fc18e
CarlosErz <rosariosalda25@gmail.com>
2023-06-23 11:30:31 -0500

    datajsModal

---
+++ src/data/dataModalLog.js
@@ -0,0 +1,10 @@
+export const dataModalLog = [
+  {
+    Title:'Errror'
+    , TipoError:'No puedes jugar sin cuenta'
+    , bt1:'Iniciar sesion'
+    , bt2:'Registrarse'
+    , link1:'/Login'
+    , link2:'/Register'
+  },
+]
--- src/pages/InicioLog.jsx
+++ src/pages/InicioLog.jsx
@@ -1,19 +1,27 @@
 import { Tematicas } from '../components/Tematicas.jsx';
 import './inicio.css';
 import compartir from '../assets/icon-compartir.svg';
-import {ModalError} from '../components/ModalError.jsx';
+import { ModalError } from '../components/ModalError.jsx';
+import { dataModalLog } from '../data/dataModalLog.js';
 
 export function InicioLog() {
   return (
     <>
-      <ModalError></ModalError>
+      <ModalError
+        Title={dataModalLog[0].Title}
+        TipoError={dataModalLog[0].TipoError}
+        bt1={dataModalLog[0].bt1}
+        bt2={dataModalLog[0].bt2}
+        link1={dataModalLog[0].link1}
+        link2={dataModalLog[0].link2}
+      />
       <div className='btn_compartir'>
         <img className='compartir' src={compartir} alt="Compartir" border="0" />
-         <a href="#" className='linkto'>
-        Con amigos es mas divertido!!
-     </a>
+        <a href="#" className='linkto'>
+          Con amigos es mas divertido!!
+        </a>
       </div>
-    
+
       <h1 className='title'>ELIGE TU TEMA</h1>
       <Tematicas></Tematicas>
     </>
