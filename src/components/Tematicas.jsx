8e6b1d3c85977f11622a3ffd10051b997e0e90a2
CarlosErz <rosariosalda25@gmail.com>
2023-06-23 12:38:30 -0500

    carruslnew

--- src/components/Tematicas.jsx
+++ src/components/Tematicas.jsx
@@ -1,48 +1,34 @@
+
 import PropTypes from 'prop-types';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 import { dataTemas } from '../data/dataTemas.js';
 
 function Tematica({ tema, imagenes, alt1 }) {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-    ],
+  const responsive = {
+    1024: { items: 3 },
+    768: { items: 2 },
+    480: { items: 2 },
+    0: { items: 1 },
   };
 
   return (
     <div className="tema-container">
       <p className="temas">{tema}</p>
       <br />
-      <Slider className='img' {...settings}>
-        {imagenes.map((imagen, imgIndex) => (
+      <AliceCarousel
+        mouseTracking
+        items={imagenes.map((imagen, imgIndex) => (
           <div className='bt' key={imgIndex}>
             <img className="Tematica" src={imagen} alt={alt1} />
             <br />
           </div>
         ))}
-      </Slider>
+        responsive={responsive}
+        autoPlay
+        autoPlayInterval={2000}
+        infinite
+      />
     </div>
   );
 }
