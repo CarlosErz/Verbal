04f1239d447111c36a8081774a50a09610955f24
CarlosErz <rosariosalda25@gmail.com>
2023-07-06 18:10:00 -0500

    fondo div animado

--- src/pages/InicioLog.jsx
+++ src/pages/InicioLog.jsx
@@ -1,64 +1,55 @@
-import { Tematicas } from '../components/Tematicas.jsx';
-import { useEffect, useState } from 'react';
+//import { Tematicas } from '../components/Tematicas.jsx';
+//import { useEffect, useState } from 'react';
 import './inicio.css';
-import compartir from '../assets/icon-compartir.svg';
-import { Juegos } from '../components/Juegos.jsx';
-import { Navbar } from '../components/Navbar.jsx';
+//import compartir from '../assets/icon-compartir.svg';
+///import { Juegos } from '../components/Juegos.jsx';
+////import { Navbar } from '../components/Navbar.jsx';
 import 'normalize.css';
-import { ModalError } from '../components/ModalError.jsx';
-import { dataModalLog } from '../data/dataModalLog.js';
+//import { ModalError } from '../components/ModalError.jsx';
+//import { dataModalLog } from '../data/dataModalLog.js';
 
 export function InicioLog() {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-
-  useEffect(() => {
-    const loggedInUser = localStorage.getItem('loggedInUser');
-    if (loggedInUser) {
-      setLoggedIn(true);
-    }
-  }, []);
-
-  const handleTematicaClick = () => {
-    if (!loggedIn) {
-      setLoggedIn(true);
-      setShowModal(true);
-    } else {
-      // Acción adicional para el caso en que el usuario esté registrado
-      // ...
-    }
-  };
 
   return (
     <>
-      <Navbar Nombre="Nombre del usuario" avatar="URL del avatar" />
-      <div className="contenido">
-        <div className='btn_compartir'>
-          <div className="bg_compartir">
-            <img className='compartir' src={compartir} alt="Compartir" />
-          </div>
-          <a href="#" className='linkto'>
-            Con amigos es más divertido!!
-          </a>
-        </div>
-        <Juegos alt1='juegos' />
+      <div className="e220_17">
+        <div className="e222_16 animation"></div>
+        <div className="e222_5 animation"></div>
+        <div className="e222_17 animation"></div>
+        <div className="e222_18 animation"></div>
+        <div className="e222_2 animation"></div>
+        <div className="e222_15 animation"></div>
+        <div className="e220_18 animation"></div>
+        <div className="e222_14 animation"></div>
+        <div className="e222_3 animation"></div>
+        <div className="e220_20 animation"></div>
+        <div className="e222_4 animation"></div>
+        <div className="e222_13 animation"></div>
+        <div className="e222_10 animation"></div>
+        <div className="e222_22 animation"></div>
+        <div className="e222_23 animation"></div>
+        <div className="e222_26 animation"></div>
+        <div className="e222_20 animation"></div>
+        <div className="e222_19 animation"></div>
+        <div className="e222_21 animation"></div>
+        <div className="e222_27 animation"></div>
+        <div className="e222_28 animation"></div>
+        <div className="e222_34 animation"></div>
+        <div className="e222_30 animation"></div>
+        <div className="e222_32 animation"></div>
+        <div className="e222_31 animation"></div>
+        <div className="e222_33 animation"></div>
+        <div className="e222_48 animation"></div>
+        <div className="e222_49 animation"></div>
+        <div className="e222_50 animation"></div>
+        <div className="e222_51 animation"></div>
+        <div className="e222_52 animation"></div>
+        <div className="e222_53 animation"></div>
+        <div className="e222_54 animation"></div>
+        <div className="e222_55 animation"></div>
+        <div className="e222_56 animation"></div>
+        <div className="e222_57 animation"></div>
       </div>
-      <p className='title'>ELIGE TU TEMA</p>
-
-
-
-      <Tematicas alt1='Imagen de Tematica' loggedIn={loggedIn} Click={handleTematicaClick} />
-
-      {showModal && !loggedIn && (
-        <ModalError
-          Title={dataModalLog[0].Title}
-          TipoError={dataModalLog[0].TipoError}
-          bt1={dataModalLog[0].bt1}
-          bt2={dataModalLog[0].bt2}
-          link1={dataModalLog[0].link1}
-          link2={dataModalLog[0].link2}
-        />
-      )}
     </>
   );
 }
---
+++ src/pages/Salas.jsx
@@ -0,0 +1,64 @@
+import { Tematicas } from '../components/Tematicas.jsx';
+import { useEffect, useState } from 'react';
+import './inicio.css';
+import compartir from '../assets/icon-compartir.svg';
+import { Juegos } from '../components/Juegos.jsx';
+import { Navbar } from '../components/Navbar.jsx';
+import 'normalize.css';
+import { ModalError } from '../components/ModalError.jsx';
+import { dataModalLog } from '../data/dataModalLog.js';
+
+export function InicioLog() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem('loggedInUser');
+    if (loggedInUser) {
+      setLoggedIn(true);
+    }
+  }, []);
+
+  const handleTematicaClick = () => {
+    if (!loggedIn) {
+      setLoggedIn(true);
+      setShowModal(true);
+    } else {
+      // Acción adicional para el caso en que el usuario esté registrado
+      // ...
+    }
+  };
+
+  return (
+    <>
+      <Navbar Nombre="Nombre del usuario" avatar="URL del avatar" />
+      <div className="contenido">
+        <div className='btn_compartir'>
+          <div className="bg_compartir">
+            <img className='compartir' src={compartir} alt="Compartir" />
+          </div>
+          <a href="#" className='linkto'>
+            Con amigos es más divertido!!
+          </a>
+        </div>
+        <Juegos alt1='juegos' />
+      </div>
+      <p className='title'>ELIGE TU TEMA</p>
+
+
+
+      <Tematicas alt1='Imagen de Tematica' loggedIn={loggedIn} Click={handleTematicaClick} />
+
+      {showModal && !loggedIn && (
+        <ModalError
+          Title={dataModalLog[0].Title}
+          TipoError={dataModalLog[0].TipoError}
+          bt1={dataModalLog[0].bt1}
+          bt2={dataModalLog[0].bt2}
+          link1={dataModalLog[0].link1}
+          link2={dataModalLog[0].link2}
+        />
+      )}
+    </>
+  );
+}
