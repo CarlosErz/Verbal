f585280959f13bd5ed5b7150ab049c316945694f
CarlosErz <rosariosalda25@gmail.com>
2023-08-21 15:28:37 -0500

    new timer and style

--- src/pages/SalaSolo.jsx
+++ src/pages/SalaSolo.jsx
@@ -2,23 +2,69 @@ import { useState, useEffect } from 'react';
 import { dataGameSolo } from '../data/dataGameSolo';
 import './Sala.css';
 import enviar from '/src/assets/Subtract.svg';
-import Countdown from 'react-countdown';
 import Confetti from 'react-confetti';
 import { Link } from 'react-router-dom';
+import logo from '/src/assets/logo.svg';
 
 export function SalaSolo() {
   const [questions, setQuestions] = useState(dataGameSolo);
   const [selectedQuestionIndexes, setSelectedQuestionIndexes] = useState([]);
+
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(null);
   const [userAnswer, setUserAnswer] = useState('');
   const [score, setScore] = useState(0);
   const [scoreerror, setScoreerror] = useState(0);
-  const [currentQuestionStartTime, setCurrentQuestionStartTime] = useState(Date.now() + 10000);
+  const [scoreTotal, setScoreTotal] = useState(0);
+
   const [ShowModalLost, setShowModalLost] = useState(false);
   const [showConfetti, setShowConfetti] = useState(false);
   const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
 
   const options = currentQuestionIndex !== null ? questions[currentQuestionIndex].options : [];
+
+
+  //barra de progreso 
+  const [time, setTime] = useState(20);
+  const initialTime = 20; // Duración inicial en segundos
+
+
+  const decreaseTime = () => {
+    if (time > 0) {
+      setTime(time - 1);
+      const progressBar = document.getElementById('myBar');
+      const progressPercentage = (time / initialTime) * 100;
+      progressBar.style.width = `${progressPercentage}%`;
+
+      if (time <= 5) {
+        progressBar.style.backgroundColor = '#f54242'; // Cambia el color a rojo cuando queda poco tiempo
+      } else if (time <= 10) {
+        progressBar.style.backgroundColor = 'rgba(255, 196, 0, 1)' // Cambia el color a amarillo cuando queda poco tiempo
+      } else {
+        progressBar.style.backgroundColor = 'rgba(46, 204, 113, 1)'; // Establece el color de la barra en verde
+      }
+    } else {
+      const progressBar = document.getElementById('myBar');
+      progressBar.style.width = '0%'; // Establece la barra en 0% visualmente
+      setTimeout(() => {
+        setShowModalLost(true);
+      }, 1000);
+    }
+  };
+
+  useEffect(() => {
+    const timer = setInterval(decreaseTime, 1000); // Decrementa el tiempo cada segundo
+    return () => {
+      clearInterval(timer); // Limpia el intervalo cuando el componente se desmonta
+    };
+  }, [time]);
+
+  const resetTime = () => {
+    setTime(initialTime);
+    const progressBar = document.getElementById('myBar');
+    progressBar.style.width = '100%';
+    progressBar.style.backgroundColor = 'rgba(46, 204, 113, 1)';
+  };
+
 
   const getRandomQuestionIndex = () => {
     const availableIndexes = questions.map((_, index) => index).filter(index => !selectedQuestionIndexes.includes(index));
@@ -35,6 +81,7 @@ export function SalaSolo() {
       setSelectedQuestionIndexes([...selectedQuestionIndexes, randomIndex]);
       setCurrentQuestionIndex(randomIndex);
       setUserAnswer('');
+
     } else {
       setShowModalLost(true);
     }
@@ -42,33 +89,33 @@ export function SalaSolo() {
 
   const checkAnswer = () => {
     const correctAnswer = questions[currentQuestionIndex].correctAnswer;
-
-
+    let newScoreTotal = scoreTotal;
+  
     if (userAnswer === correctAnswer) {
       setScore(score + 1);
+      newScoreTotal += 1000;
       setIsAnswerCorrect(true);
       setShowConfetti(true);
-
-      setTimeout(() => {
-        setShowConfetti(false);
-        goToNextRandomQuestion();
-        setIsAnswerCorrect(null);
-        setCurrentQuestionStartTime(Date.now() + 10000);
-      }, 1000);
     } else {
+      newScoreTotal = Math.max(0, newScoreTotal - 500);
       setScoreerror(scoreerror + 1);
       setIsAnswerCorrect(false);
-      setTimeout(() => {
-        goToNextRandomQuestion();
-        setIsAnswerCorrect(null);
-        setCurrentQuestionStartTime(Date.now() + 10000);
-      }, 1000);
     }
+  
+    setScoreTotal(newScoreTotal);
+  
+    setTimeout(() => {
+      setShowConfetti(false);
+      goToNextRandomQuestion();
+      setIsAnswerCorrect(null);
+      resetTime();
+    }, 1000);
   };
+  
   const handleAnswerSelection = (answer) => {
     setUserAnswer(answer);
   };
-  
+
 
   const restartGame = () => {
     setSelectedQuestionIndexes([]);
@@ -77,14 +124,9 @@ export function SalaSolo() {
     setScore(0);
     setScoreerror(0);
     setShowModalLost(false);
-    setCurrentQuestionStartTime(Date.now() + 10000);
+    resetTime();
     location.reload();
 
-  };
-
-  const handleCountdownComplete = () => {
-    goToNextRandomQuestion();
-    setShowModalLost(true);
   };
 
   useEffect(() => {
@@ -97,45 +139,39 @@ export function SalaSolo() {
     }
   }, [selectedQuestionIndexes, currentQuestionIndex]);
 
+
   return (
     <div className="SalaSolo">
-      {ShowModalLost && 
+      {ShowModalLost &&
         <div className="lostGame">
-        <div className='lostGame_content'>
-        <h1 className='lostGame_title'>¡Oops, Tiempo Agotado!</h1>
-        <p className='lostGame_text'>No te desanimes, sigue desafiándote</p>
-        <p className='lostGame_puntaje'>Tu puntaje fue de</p>
-        <div className="losGame_score">
-          
-          <div className="scores">
-            <div className="score">
-              <p>✓</p>
-              <span>{score}</span>
+          <div className='lostGame_content'>
+            <h1 className='lostGame_title'>¡Oops, Tiempo Agotado!</h1>
+            <p className='lostGame_text'>No te desanimes, sigue desafiándote</p>
+            <p className='lostGame_puntaje'>Tu puntaje fue de</p>
+            <div className="losGame_score">
+
+              <div className="scores">
+                <div className="score">
+                  <p>✓</p>
+                  <span>{score}</span>
+                </div>
+                <div className="score_negative">
+                  <p>✖</p>
+                  <span>{scoreerror}</span>
+                </div>
+              </div>
             </div>
-            <div className="score_negative">
-              <p>✖</p>
-              <span>{scoreerror}</span>
-            </div>
+            <Link to='/SalaSolo' onClick={restartGame}>Volver a jugar</Link>
           </div>
         </div>
-        <Link to='/SalaSolo' onClick={restartGame}>Volver a jugar</Link>
-        </div>
-      </div>
       }
-      
+
       {showConfetti && <Confetti />}
       <nav className="SalaNav">
-        <Link to='/'>Ruleta De Preguntas</Link>
-        <div className="scores">
-          <div className="score">
-            <p>✓</p>
-            <span>{score}</span>
-          </div>
-          <div className="score_negative">
-            <p>✖</p>
-            <span>{scoreerror}</span>
-          </div>
-        </div>
+        
+        <Link to='/'><img className='logo' src={logo} alt="" /></Link>
+        <p>Ruleta De Preguntas</p>
+      
 
       </nav>
       <div className="SalaContent">
@@ -144,21 +180,23 @@ export function SalaSolo() {
             <h2 className="SalaQuestion">
               {currentQuestionIndex !== null ? questions[currentQuestionIndex].question : "Cargando pregunta..."}
             </h2>
-            <section className="SalaTime">
-              <h2 className="SalaTitleTime">SEGUNDOS</h2>
-              <div className="SalaTimeContent">
-                <Countdown
-                  date={currentQuestionStartTime}
-                  onComplete={handleCountdownComplete}
-                  renderer={({ seconds }) => <p className="SalaTimer">{seconds}</p>}
-                />
-              </div>
-            </section>
+            <div className="scores">
+          <div className="SalaPuntaje">
+            <p>Puntaje</p>
+            <span>{scoreTotal}</span>
           </div>
+        </div>
+            <div className="progress-container ">
+              <div className="progress-bar" id="myBar"></div>
+            </div>
+
+
+          </div>
+
           <div className="SalaAnswersContent">
             {options.slice(0, 2).map((option, index) => (
               <div className="SalaAnswer" key={index} onClick={() => handleAnswerSelection(option)}>
-                <span className="SalaInciso">{String.fromCharCode(65 + index)}) </span>
+                <span className="SalaInciso">{String.fromCharCode(65 + index)}</span>
                 <button className={`SalaBtn ${isAnswerCorrect === true ? 'green' : isAnswerCorrect === false ? 'red' : ''}`}>
                   {option}
                 </button>
@@ -168,7 +206,7 @@ export function SalaSolo() {
           <div className="SalaAnswersContent">
             {options.slice(2, 4).map((option, index) => (
               <div className="SalaAnswer" key={index + 2} onClick={() => handleAnswerSelection(option)}>
-                <span className="SalaInciso">{String.fromCharCode(67 + index)}) </span>
+                <span className="SalaInciso">{String.fromCharCode(67 + index)}</span>
                 <button className={`SalaBtn ${isAnswerCorrect === true ? 'green' : isAnswerCorrect === false ? 'red' : ''}`}>
                   {option}
                 </button>
