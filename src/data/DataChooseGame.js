4338603ca01826afc6adcec1fdbd45f22a95425a
CarlosErz <rosariosalda25@gmail.com>
2023-09-05 19:18:02 -0500

    choosegame img

--- src/data/DataChooseGame.js
+++ src/data/DataChooseGame.js
@@ -1,19 +1,19 @@
 export const dataChooseGame = [
   {
     Game: '/QuickGame',
-    image: 'https://i.postimg.cc/ZY8rWdvf/CULTURA.jpg',
+    image: 'https://i.postimg.cc/L5SnqJxm/CULTURA.jpg',
   },
   {
     Game: '/QuickDisney',
-    image: 'https://i.postimg.cc/cHYfMK9L/DISNEY.jpg',
+    image: 'https://i.postimg.cc/NMmKf67N/DISNEY.jpg',
   },
   {
     Game: '/QuickEnfermeria',
-    image: 'https://i.postimg.cc/tJNPwKH0/ENFERMERIA.jpg',
+    image: 'https://i.postimg.cc/D0cm17NL/ENFERMERIA.jpg',
   },
   {
     Game: '/QuickAnimals',
-    image: 'https://i.postimg.cc/3wZQJXZ3/ANIMALES.jpg',
+    image: 'https://i.postimg.cc/jjGWbgNw/animal.jpg',
   }
 
 ];
