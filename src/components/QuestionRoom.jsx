f18fc4328caa24df4dd3abe72d014f55154f4fcc
CarlosErz <rosariosalda25@gmail.com>
2024-10-25 18:58:55 -0500

    Refactor time decrement logic in QuestionRoom component

--- src/components/QuestionRoom.jsx
+++ src/components/QuestionRoom.jsx
@@ -65,11 +65,11 @@ export function QuestionRoom({ questionData }) {
       let timeDecrementFactor;
 
       if (lives === 3) {
-        timeDecrementFactor = 1.5;
-      } else if (lives === 2) {
         timeDecrementFactor = 2;
+      } else if (lives === 2) {
+        timeDecrementFactor = 3.5;
       } else {
-        timeDecrementFactor = 3;
+        timeDecrementFactor = 4;
       }
       if (time > 0) {
         setTime(time - timeDecrementFactor);
